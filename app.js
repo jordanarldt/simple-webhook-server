42a8aff466f6335d93f2957e62d0f90e985ba42c
Jordan Arldt <jordanarldt@gmail.com>
2021-04-24 17:10:59 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.post("/webhooks", (req, res) => {
+  console.log(req.body);
+  
+  res.status(200).send("Webhook received");
+});
+
+app.get("/", (req, res) => {
+  res.send("Webhooks testing app. Set webhook destination URLs to the /webhook path");
+});
+
+var listener = app.listen(process.env.PORT || 8080, function() {
+  console.log(`Listening on port ${listener.address().port}`);
+});
