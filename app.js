60acc3c0b2b4b10ab2179e715bfa313ea48d5426
Jordan Arldt <jordan.arldt@bigcommerce.com>
2021-10-05 14:48:38 -0500

    Minor change

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.post("/webhooks", (req, res) => {
+  res.status(200).send("Webhook received");
   console.log(`RECEIVED HOOK FROM ${req.body.scope}`);
   console.log(req.body);
-  res.status(200).send("Webhook received");
 });
 
 app.get("/", (req, res) => {
